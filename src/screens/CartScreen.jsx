0b84622ec9df839f3c682ee1b6c455aa09468a5a
Alexei Rusu <rusualexei1995@gmail.com>
2023-11-06 20:54:11 -0500

    remove btn from cart

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import { FaTrash } from 'react-icons/fa'
 import Message from '../components/Message'
-import { addToCart } from '../slices/cartSlice'
+import { addToCart, removeFromCart } from '../slices/cartSlice'
 
 function CartScreen() {
   const navigate = useNavigate()
@@ -15,6 +15,11 @@ function CartScreen() {
   const addToCartHandler = async (product, qty) => {
     dispatch(addToCart({...product, qty}))
   }
+
+  const removeFromCartHandler = async (id) => {
+    dispatch(removeFromCart(id))
+  }
+
 
   return (
     <Row>
@@ -54,7 +59,7 @@ function CartScreen() {
                                             </Form.Control>
                                         </Col>
                                         <Col md={2}>
-                                            <Button type='button' variant='light'>
+                                            <Button type='button' variant='light' onClick={() => removeFromCartHandler(item._id)}>
                                                 <FaTrash />
                                             </Button>
                                         </Col>
