87330907cdd758bbb3ac0815c142882d6ce25852
Alexei Rusu <rusualexei1995@gmail.com>
2023-11-10 12:58:56 -0500

    able to log in

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,16 +1,44 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { Link, useLocation, useNavigate } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
 import { Form, Button, Row, Col } from "react-bootstrap"
 import FormContainer from "../components/FormContainer"
-
+import Loader from "../components/Loader"
+import { useLoginMutation } from "../slices/usersApiSlice"
+import { setCredentials } from "../slices/authSlice"
+import { toast } from 'react-toastify'
 
 function LoginScreen() {
 const [ email, setEmail ] = useState('')
 const [password, setPassword ] = useState('')
 
-const submitHandler = (e) => {
+const dispatch = useDispatch()
+const navigate = useNavigate()
+
+const [ login, {isLoading} ] = useLoginMutation()
+
+const { userInfo } = useSelector((state) => state.auth)
+
+const { search } = useLocation()
+const sp = new URLSearchParams(search)
+const redirect = sp.get('redirect') || '/'
+
+ useEffect(() => {
+  if(userInfo) {
+    navigate(redirect)
+  }
+}, [userInfo, redirect, navigate])
+
+
+const submitHandler = async (e) => {
     e.preventDefault()
-    console.log('submit')
+     try {
+        const res = await login({email, password}).unwrap()
+        dispatch((setCredentials({...res,})))
+        navigate(redirect)
+     } catch (error) {
+        toast.error(error?.data?.message || error.data)
+     }
 }
 
 
@@ -38,14 +66,19 @@ const submitHandler = (e) => {
                     >
                 </Form.Control>
             </Form.Group>
-            <Button type='submit' variant="primary" className="mt-2" >
+
+            <Button type='submit' variant="primary" className="mt-2" disabled={ isLoading }>
                 Sign In
             </Button>
+
+            { isLoading && <Loader />}
+
+
         </Form>
 
         <Row className="py-3">
             <Col>
-                New Costumer? <Link to='/register' >Register</Link>
+                New Costumer? <Link to={ redirect ? `/register?redirect=${redirect}` : '/register'} >Register</Link>
             </Col>
         </Row>
 
