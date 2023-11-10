a9396c2a831ac4d1569cd33fbfb12f48a9f6f210
Alexei Rusu <rusualexei1995@gmail.com>
2023-11-10 13:18:22 -0500

    login functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Badge, Navbar, Nav, Container } from 'react-bootstrap'
+import { Badge, Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
 import { useSelector } from 'react-redux'
@@ -7,7 +7,12 @@ import logo from '../assets/logo.png'
 
 function Header() {
   const { cartItems } = useSelector((state) => state.cart) 
-  console.log(cartItems)
+  const { userInfo } = useSelector((state) => state.auth) 
+ 
+
+ const logoutHandler = () => {
+    console.log('logout')
+ }
 
   return (
     <header style={{marginBottom: '25px'}}>
@@ -34,9 +39,23 @@ function Header() {
                             }
                             </Nav.Link>                      
                         </LinkContainer>
-                        <LinkContainer to='/login'>
-                            <Nav.Link ><FaUser /> Sign In</Nav.Link>                        
-                        </LinkContainer>
+
+                            { userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to='/login'>
+                                    <Nav.Link ><FaUser /> Sign In</Nav.Link>                        
+                                </LinkContainer>
+                            ) }
+
+
 
                     </Nav>
                 </Navbar.Collapse>
